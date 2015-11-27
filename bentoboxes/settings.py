9ceb159e7e650ffecadfa97b2008298b5c96e4b5
Antonia Blair <antoniablair@gmail.com>
2015-11-26 23:06:51 -0500

    Use @login_required decorator to require logins

--- bentoboxes/settings.py
+++ bentoboxes/settings.py
@@ -91,3 +91,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Login redirects to root
+LOGIN_REDIRECT_URL = '/'
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -16,6 +17,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -33,6 +35,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     #  get the Post model we want to edit, with pk
     post = get_object_or_404(Post, pk=pk)
@@ -55,11 +58,13 @@ def post_edit(request, pk):
 
 
 # View drafts
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     # use publish method
@@ -67,6 +72,7 @@ def post_publish(request, pk):
     return redirect('blog.views.post_detail', pk=pk)
 
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
