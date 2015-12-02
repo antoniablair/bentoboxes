f7d98f2b3425caf141ae9d01aad0185a83b4b00d
Antonia Blair <antoniablair@gmail.com>
2015-12-01 23:33:37 -0500

    Reverse order of blogs and recipes (show by latest)

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Recipe
 
 
 def recipes_list(request):
-    recipes = Recipe.objects.all()
+    recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by(
+        'published_date').reverse()
+
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes })
