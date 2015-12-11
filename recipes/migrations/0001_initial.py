020ac6231eae3e11bf1fd0185d3ae080bd3a9dec
Antonia Blair <antoniablair@gmail.com>
2015-12-11 00:20:54 -0500

    Dec-9: Migrations

---
+++ recipes/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Recipe',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=400)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('published_date', models.DateTimeField(null=True, blank=True)),
+                ('is_featured', models.BooleanField(default=False)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('likes', models.ManyToManyField(related_name='likes', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
---
+++ recipes/migrations/0005_like.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('recipes', '0004_auto_20151202_2357'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Like',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('creator', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
