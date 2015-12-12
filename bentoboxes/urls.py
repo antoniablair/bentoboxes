9318e26723a85042960a564a205dd1c0ecb5bedd
Antonia Blair <antoniablair@gmail.com>
2015-12-12 16:14:04 -0500

    12-12: Finishes part 1 of REST tutorial

--- bentoboxes/urls.py
+++ bentoboxes/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     url(r'', include('recipes.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-    url(r'^api/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include('snippets.urls')),
 ]
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from snippets import views
 
+# Needs better error handling for if json is malformed or request is made with method view doesn't handle
+
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
-]
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),]
