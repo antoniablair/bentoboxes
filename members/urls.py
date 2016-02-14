1ba7b5dd8ef595f49b06088c4b76d6ebba210300
Antonia Blair <antoniablair@gmail.com>
2016-02-14 16:22:15 -0500

    Moves static files to shared folder

--- members/urls.py
+++ members/urls.py
@@ -3,5 +3,5 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^tonia/$', views.member_profile, name='member_profile'),
+    url(r'^profile/$', views.member_profile, name='member_profile'),
 ]
