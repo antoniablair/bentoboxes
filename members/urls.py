64d02ba4166d6b21a334f12a2f89c7a153f3ba4c
Antonia Blair <antoniablair@gmail.com>
2016-02-14 16:57:00 -0500

    Adds case insensitive member profile urls

--- members/urls.py
+++ members/urls.py
@@ -3,5 +3,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^profile/$', views.member_profile, name='member_profile'),
+    url(r'^profile/(?P<name>\w+)/$', views.member_profile, name='member_profile'),
 ]
+#
+# url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
