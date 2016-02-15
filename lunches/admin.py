72409f2355b7c662223a4091441f6faefe5ccbaa
Antonia Blair <antoniablair@gmail.com>
2016-02-14 19:56:01 -0500

    Adds inline image uploading option

--- lunches/admin.py
+++ lunches/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Lunch, Food, LunchImage
 
-# Register your models here.
+class LunchImageInline(admin.TabularInline):
+    model = LunchImage
+    fk_name = 'lunch'
+
+class LunchAdmin(admin.ModelAdmin):
+    inlines = [ LunchImageInline, ]
+
+
+admin.site.register(Lunch, LunchAdmin)
--- lunches/models.py
+++ lunches/models.py
@@ -12,19 +12,34 @@ from django.utils import timezone
 
 class Food(models.Model):
     name = models.CharField(max_length=200, default='apple')
-    recipe = models.ForeignKey(Recipe, blank=True)
+    needs_instructions = models.BooleanField(default=False, help_text=(u'Does this lunch addition require '
+                                                                       u'instructions (e.g., a sandwich?) '
+                                                                       u'Choose yes or no.'))
+    recipe = models.ForeignKey(Recipe, blank=True, null=True, help_text=(u'If instructions are necessary, include '
+                                                                         u'the recipe here.'))
+
+    def __unicode__(self):
+        return self.name
 
 
 class Lunch(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=400, default='My lunch')
     tagline = models.TextField(max_length=100)
-    description = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     is_featured = models.BooleanField(default=False)
-    foods = models.ManyToManyField(Food, related_name='lunches')
+    foods = models.ManyToManyField(Food, related_name='lunches', blank=True)
 
     def __unicode__(self):
         return self.title
+
+
+class LunchImage(models.Model):
+    lunch = models.ForeignKey(Lunch, related_name='images')
+    image = models.ImageField(upload_to='images/')
+
+    def __unicode__(self):
+        return self.lunch.title
 
 
